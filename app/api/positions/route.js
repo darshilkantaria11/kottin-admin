449881ed84a7301cf0828a1b2a2e197d7ff1be34
darshilkantaria11 <darshilkantaria11@gmail.com>
2024-10-23 22:54:36 +0530

    up

--- app/api/positions/route.js
+++ app/api/positions/route.js
@@ -1,4 +1,3 @@
-// /app/api/positions/route.js
 import { dbConnect } from '../../utils/mongoose';
 import Position from '../../models/position';
 import { NextResponse } from 'next/server';
@@ -7,7 +6,14 @@ export async function GET() {
     try {
         await dbConnect();
         const positions = await Position.find(); // Fetch all positions from the database
-        return NextResponse.json(positions);
+        const response = NextResponse.json(positions, { status: 200 });
+
+        // Set headers to prevent caching
+        response.headers.set('Cache-Control', 'no-store'); // Prevent caching
+        response.headers.set('Pragma', 'no-cache'); // HTTP 1.0
+        response.headers.set('Expires', '0'); // Proxies
+
+        return response;
     } catch (error) {
         console.error('Error:', error.message);
         return NextResponse.json({ error: error.message }, { status: 500 });
