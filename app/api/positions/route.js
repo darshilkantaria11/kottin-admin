8214b5b1e707ae550a8ee48efdf9fc55ae4236af
darshilkantaria11 <darshilkantaria11@gmail.com>
2024-11-23 16:03:40 +0530

    up

--- app/api/positions/route.js
+++ app/api/positions/route.js
@@ -3,20 +3,8 @@ import Position from '../../models/position';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-    try {
+    
         await dbConnect();
-        console.log("Fetching positions from MongoDB...");
-        const positions = await Position.find(); // Fetch all positions from the database
-        console.log(`Found ${positions.length} positions.`);
-        
-        return NextResponse.json(positions, {
-            headers: {
-                // Prevent caching and revalidate on every request
-                'Cache-Control': 'public, max-age=0, must-revalidate, stale-while-revalidate=60'
-            }
-        });
-    } catch (error) {
-        console.error('Error:', error.message);
-        return NextResponse.json({ error: error.message }, { status: 500 });
-    }
+        const positions = await Position.find(); // Fetch all positions from the database   
+        return NextResponse.json(positions);
 }
